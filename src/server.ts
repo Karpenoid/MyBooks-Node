bd5542e22c1d98a8e066aa82d9d67b823aa3ca28
Karpenoid <mxkarpen@gmail.com>
2026-06-21 23:28:01 +0300

    Installed Eslint, added env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
-import "dotenv/config";
+import { PORT } from "./env.js";
 import app from "./app.js";
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
