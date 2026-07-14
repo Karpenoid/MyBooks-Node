51d8cb245a1c4915adaa5880d01f9643c64c58bd
Karpenoid <mxkarpen@gmail.com>
2026-07-14 17:40:30 +0300

    Added prisma singleton in lib, prisma connect & graceful shutdown in server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
 import { PORT } from "./env.js";
 import app from "./app.js";
+import { prisma } from "./lib/prisma.js";
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  await prisma.$connect();
+  console.log("Connected to the database");
+
+  app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  console.log("Disconnected from the database");
+  process.exit(0);
+})
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  console.log("Disconnected from the database");
+  process.exit(0);
+})
+
+startServer();
